1ac76ca33551438890198ac7fb1bf84bf7890af5
John Florence <codytdrew@gmail.com>
2019-07-27 11:08:24 -0700

    fixed mongo

--- myApp.js
+++ myApp.js
@@ -1,3 +1,7 @@
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI);
+
 /**********************************************
 * 3. FCC Mongo & Mongoose Challenges
 * ==================================
